a5b47e32943377a46ab484bd3b662ce8bfc866e1
EErickson <avilasonson@gmail.com>
2023-08-09 22:17:33 +0800

    :bricks: changes on codebase

--- check.py
+++ check.py
@@ -1,27 +1,22 @@
 import sqlite3
 import json
+import pymysql
+from datetime import datetime
+#Global Var
+beanArray = []
+
 # Connect to the database
 conn = sqlite3.connect('beanCounter.db')
 cursor = conn.cursor()
-beanArray = []
 
-def allFetchData():
-    # Execute a SELECT query to retrieve data
-    select_query = '''
-    SELECT * FROM beans;
-    '''
-    cursor.execute(select_query)
-
-    # Fetch all rows returned by the query
-    rows = cursor.fetchall()
-
-    # Check if any rows are returned
-    if len(rows) > 0:
-        print("Data is stored in the database.")
-        for row in rows:
-            print(row)  # Print each row
-    else:
-        print("No data found in the database.")
+#mysql file
+mysqlConn = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="coffee_fnl"
+)
+mysqlCursor = mysqlConn.cursor()
 
 def fetchBeanCount():
     global beanArray
@@ -45,12 +40,26 @@ def fetchBeanCount():
     print(beanArray)
 
 fetchBeanCount()
+#sqlite to json format
+
 dataDict = {
     "good": beanArray[0],
-    "bad": beanArray[1]
+    "bad": beanArray[1],
+    "kilograms": ((beanArray[0] + beanArray[1]) * 0.2) / 1000 
 }
 
 with open('beanData.json', 'w') as json_file:
     json.dump(dataDict, json_file)
 
+json_data = json.dumps(dataDict)
+goodBean = beanArray[0]
+badBean = beanArray[1]
+kiloOfBeans = ((beanArray[0] + beanArray[1]) * 0.2) / 1000
+formattedKilo = "{:.4f}".format(kiloOfBeans)
+createdAt = datetime.now()
+updatedAt = createdAt
+insert_query = "INSERT INTO bean_counts (good, bad, kilograms, created_at, updated_at) VALUES (%s, %s, %s, %s, %s);"
+mysqlCursor.execute(insert_query, (goodBean, badBean, formattedKilo, createdAt, updatedAt))
+mysqlConn.commit()
+mysqlConn.close()
 conn.close()
